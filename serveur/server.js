ec1a7bffe54f91e30e493186506dc6e5cb98e740
Marvin MOTTET <mottet.marvin@gmail.com>
2018-01-14 23:40:44 +0100

    use public folder for server

--- serveur/server.js
+++ serveur/server.js
@@ -2,6 +2,8 @@ var express = require('express');
 var server = express();
 var path = require('path');
 
+server.use(express.static(__dirname + '/public'));
+
 var mysql = require('mysql')
 var pool = mysql.createPool({
     host: 'localhost',
@@ -10,9 +12,9 @@ var pool = mysql.createPool({
     database: 'iot_project'
 })
 
-server.get('/', function(req, res) {
+/*server.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + "/index.html"));
-});
+});*/
 
 server.get('/validations', function(req, res) {
     pool.getConnection(function(err, connection) {
